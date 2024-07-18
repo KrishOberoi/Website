a3a78f3e6e2532968949ffc763f9a3a7f0f14e6c
Krish <75732836+KrishOberoi@users.noreply.github.com>
2024-07-18 13:41:08 +0000

    2nd commit

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+jobs = [
+  {   
+    'id': 1,
+    'title': 'Data Analyst',
+    'salary': 'Rs. 10,00,000'
+  },
+  {  
+    'id': 2,
+    'title': 'sofware engineer',
+    'salary': 'Rs. 12,90,999'
+  }
+]
 
 @app.route('/')
 def hello_world():
-  return 'Hello p!'
+  return render_template('home.html', name='Krish',job=jobs)
 
 
-print(__name__)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
